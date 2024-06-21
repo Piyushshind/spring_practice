2e744a4f4a4a25c4f998c93c762e6d3623705aae
Piyushshind <piyushshinde.199@gmail.com>
2024-06-21 17:19:30 +0530

    form mapping

--- demo/src/main/java/com/example/demo/myCotroller.java
+++ demo/src/main/java/com/example/demo/myCotroller.java
@@ -2,13 +2,19 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 public class myCotroller {
     @RequestMapping("ps")
-
     public static String myMethod() {
         return "hello piyush ";
+    }
+
+    @PostMapping("formData")
+    public static String getData(@RequestParam int num1, @RequestParam int num2) {
+        int res = num1 + num2;
+        return "Result is :- " + res;
     }
 }
